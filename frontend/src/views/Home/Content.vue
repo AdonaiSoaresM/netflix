1083527ab90d13f9f391fd014ddcf3402348d309
adonai.m <adonai.m@lyncas.net>
2023-03-21 17:36:16 -0300

    MovieRom Component and Integration API Movie

--- frontend/src/views/Home/Content.vue
+++ frontend/src/views/Home/Content.vue
@@ -1,13 +1,38 @@
 <template lang="">
-    <div>
-        
+    <div class="content">
+        <h1>Filmes Disponíveis</h1>
+        <MovieRow :movies="movies"/>
+        <h1>Recomendados para Você</h1>
+        <MovieRow :movies="movies"/>
     </div>
 </template>
 <script>
+import { getMovies } from '@/common/service/tmdb.service';
+import MovieRow from './MovieRow.vue';
+
 export default {
-    
+    components: { MovieRow },
+    name: "Content",
+    data() {
+        return {
+            movies: []
+        }
+    },
+    async mounted(){
+        let response = await getMovies();
+        this.movies = response.items
+    }
 }
 </script>
-<style>
-    
+<style scoped>
+
+.content {
+    overflow: auto;
+}
+
+
+h1 {
+    color: #FFF;
+    margin-left: 30px;
+}
 </style>
--- frontend/src/views/Home/Home.vue
+++ frontend/src/views/Home/Home.vue
@@ -1,13 +1,15 @@
 <template lang="">
   <div class="home">
     <Header />
+    <Content />
   </div>
 </template>
 <script>
 import Header from "./Header.vue";
+import Content from "./Content.vue";
 
 export default {
-  components: { Header },
+  components: { Header, Content },
   name: "Home",
 };
 </script>
@@ -16,6 +18,7 @@ export default {
     font-family: "Roboto", sans-serif;
     background-color: rgb(25, 25, 25);
     height: 100%;
+    overflow: auto;
   }
 
   h1 {
