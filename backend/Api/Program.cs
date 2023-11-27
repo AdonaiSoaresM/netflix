6f5aaceb1ecce9f58f7e9914fa667b4e64aa8bd8
AdonaiSoaresM <adonaielite@gmail.com>
2023-11-27 18:07:52 -0300

    Swagger prod

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -56,11 +56,8 @@ var app = builder.Build();
 app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
